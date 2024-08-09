d430f7f89aa4b15c62edd92c4ef86d8ff4444de2
Julian Gonzalez <gonzalez.escobar.julian.andres@gmail.com>
2024-08-09 10:26:39 -0500

    Creacion archivo js

---
+++ script.js
@@ -0,0 +1,10 @@
+const text__area =  document.getElementsByClassName("textarea__contenedor__texto__a__encriptar");
+const text__area_mensaje = document.getElementsByClassName("textarea__contenedor__texto__encriptado");
+
+
+function encriptar(){
+
+    console.log(text__area.value)
+
+
+}
